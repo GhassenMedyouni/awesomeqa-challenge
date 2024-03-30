a3541378f3fdbf35da9582f793cadfc175e705de
Ghassen <ghassen.medyouni@gmail.com>
2024-03-30 15:09:39 +0100

    fix: fix the type hinting issue

--- backend/app/repositories/ticket_repository.py
+++ backend/app/repositories/ticket_repository.py
@@ -7,5 +7,5 @@ class TicketRepository:
         with open(filepath) as json_file:
             self.data = json.load(json_file)
 
-    def get_tickets(self, limit: Optional[int] = None) -> list[dict]:
+    def get_tickets(self, limit: Optional[int] = None) -> list:
         return self.data["tickets"][:limit]
