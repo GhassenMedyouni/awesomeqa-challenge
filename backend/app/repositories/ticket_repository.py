7053e4dcfea65ec55fccfcd8b6ed5789777eee29
Ghassen <ghassen.medyouni@gmail.com>
2024-04-02 16:34:28 +0200

    update ticket

--- backend/app/repositories/ticket_repository.py
+++ backend/app/repositories/ticket_repository.py
@@ -12,9 +12,14 @@ class TicketRepository:
 
     def update_ticket_by_id(self, id_to_update: str, updated_data: dict):
         for ticket in self.data["tickets"]:
+            ticket_found = False
             if ticket["id"] == id_to_update:
                 ticket.update(updated_data)
+                ticket_found = True
                 break
+
+        if not ticket_found:
+            raise Exception(f"Ticket with ID {id_to_update} not found.")
 
         # Write the updated data back to the JSON file
         with open(self.filepath, 'w') as file:
